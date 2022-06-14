75602426db6a28babdcf6d52553d5d4ab56e2e92
ashinde1760 <ashinde1760@gmail.com>
2022-06-14 15:22:35 +0530

    Updated code with bookmark

--- src/app/Components/headers/navbar/bookmarks/bookmarks.component.ts
+++ src/app/Components/headers/navbar/bookmarks/bookmarks.component.ts
@@ -1,17 +1,22 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { DocumentUploadService } from 'src/app/Services/document-upload.service';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-bookmarks',
   templateUrl: './bookmarks.component.html',
-  styleUrls: ['./bookmarks.component.css']
+  styleUrls: ['./bookmarks.component.css'],
+  providers: [ConfirmationService, MessageService],
+
 })
 export class BookmarksComponent implements OnInit {
 
   bookmarks:any=[];
 
 
-  constructor(private service: DocumentUploadService) { }
+  constructor(private service: DocumentUploadService,private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.service.getBookmarks().subscribe(
@@ -23,6 +28,31 @@ export class BookmarksComponent implements OnInit {
         alert("something went wrong, please try again later");
       }
     )
+
+    
   }
 
+  download(id:string){
+    this.service.download(id).subscribe(
+      (event:any)=>{
+        saveAs(event, id);
+        // if(data.status===200)
+        // {
+        //   this.messageService.add({
+        //     severity: 'success',
+        //     summary: 'Confirmed',
+        //     detail:
+        //       'File Uploaded successfully, refresh page to see document',
+        //   });
+        // }
+      },
+      (error:HttpErrorResponse)=>{
+
+      }
+    )
+  }
+
+  cancleBookmark(id:string){
+
+  }
 }
